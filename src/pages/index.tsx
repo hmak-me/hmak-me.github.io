c0b526cf75cc5704fc53f5b304ccd18ccf4713d4
Hossein Maktoobian <8bitsight@gmail.com>
2020-12-07 15:32:07 +0330

    Add social links to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,15 +10,24 @@ const HomePage = () => (
     <SEO title="Home" />
 
     <div className="container">
-      <div className={styles.info}>
-        <div className={styles.hi}>Hi, I'm</div>
-        <div className={styles.name}>Hossein</div>
-        <div className={styles.jobTitle}>Web Developer</div>
-      </div>
+      <aside>
+        <div className={styles.info}>
+          <div className={styles.hi}>Hi, I'm</div>
+          <div className={styles.name}>Hossein</div>
+          <div className={styles.jobTitle}>Web Developer</div>
 
-      <Link to='/cv'>
-        <Button>Download CV</Button>
-      </Link>
+          <Link to='/cv'>
+            <Button>Download CV</Button>
+          </Link>
+        </div>
+
+        <ul className={styles.socials}>
+          <li className={styles.social}><a href="https://github.com/hmak-me">GitHub</a></li>
+          <li className={styles.social}><a href="https://instagram.com/hmak.me">Instagram</a></li>
+          <li className={styles.social}><a href="https://twitter.com/hmakme">Twitter</a></li>
+          <li className={styles.social}><a href="https://linkedin.com/in/hmak-me/">LinkedIn</a></li>
+        </ul>
+      </aside>
     </div>
   </Layout>
 )
