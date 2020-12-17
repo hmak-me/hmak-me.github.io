be64dd0855ff622d7cd444ebd5ced906a027911f
Hossein Maktoobian <atikalaker@gmail.com>
2020-12-17 13:23:47 +0330

    Finalize CV Layout

--- src/pages/cv/index.tsx
+++ src/pages/cv/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../../layout"
 import SEO from "../../components/seo"
-import styles from "./index.module.styl"
 import GitHubIcon from "../../assets/icons/github"
 import TwitterIcon from "../../assets/icons/twitter"
 import LinkedInIcon from "../../assets/icons/linkedin"
@@ -14,6 +13,7 @@ import BookIcon from "../../assets/icons/book"
 import UserIcon from "../../assets/icons/user"
 import ShareIcon from "../../assets/icons/share"
 import ContactIcon from "../../assets/icons/contact"
+import styles from "./index.module.styl"
 
 
 const CVPage = () => {
@@ -23,13 +23,13 @@ const CVPage = () => {
 
       <div className={`container ${styles.cv}`}>
         <header className={styles.header}>
-          <section className={styles.info}>
+          <div className={styles.info}>
             <div className={styles.name}>
               <span className={styles.firstname}>Hossein</span>
               <span className={styles.lastname}>Maktoobian</span>
             </div>
             <div className={styles.jobTitle}>Full Stack Web Developer</div>
-          </section>
+          </div>
         </header>
 
 
@@ -37,7 +37,7 @@ const CVPage = () => {
           <section className={styles.profile}>
             <header>
               <UserIcon />
-              <h3>Profile</h3>
+              <h2>Profile</h2>
             </header>
 
             <p>Self taught programmer who is eager to learn more and more everyday. I got engaged with computers when I was 5, that was the
@@ -48,45 +48,45 @@ const CVPage = () => {
           <section className={styles.education}>
             <header>
               <BookIcon />
-              <h3>Education</h3>
+              <h2>Education</h2>
             </header>
 
-            <div>
-              <div className={styles.date}>Sep 23, 2015 - Jan 20, 2018</div>
-              <div className={styles.field}>Associate of Software Engineering</div>
-              <div className={styles.university}>Mohajer Technical and Vocational College</div>
-            </div>
+            <article>
+              <h5>Sep 23, 2015 - Jan 20, 2018</h5>
+              <h4>Mohajer Technical and Vocational College</h4>
+              <h3>Associate of Software Engineering</h3>
+            </article>
 
-            <div>
-              <div className={styles.date}>Sep 23, 2019 - Now</div>
-              <div className={styles.field}>Bachelor of Software Engineering</div>
-              <div className={styles.university}>Shamsipour Technical and Vocational College</div>
-            </div>
+            <article>
+              <h5>Sep 23, 2019 - Now</h5>
+              <h4>Shamsipour Technical and Vocational College</h4>
+              <h3>Bachelor of Software Engineering</h3>
+            </article>
           </section>
 
           <section className={styles.awards}>
             <header>
               <TrophyIcon />
-              <h3>Awards</h3>
+              <h2>Awards</h2>
             </header>
 
-            <div>
-              <div className={styles.date}>Jul 28, 2018</div>
-              <div className={styles.info}>18th Iran National Skills</div>
-              <div className={styles.award}>Gold Medal</div>
-            </div>
+            <article>
+              <h5>Jul 28, 2018</h5>
+              <h4>18th Iran National Skills</h4>
+              <h3>Gold Medal</h3>
+            </article>
 
-            <div>
-              <div className={styles.date}>Aug 27, 2019</div>
-              <div className={styles.info}>45th WorldSkills Competition</div>
-              <div className={styles.award}>Medallion Of Excellence</div>
-            </div>
+            <article>
+              <h5>Aug 27, 2019</h5>
+              <h4>45th WorldSkills Competition</h4>
+              <h3>Medallion Of Excellence</h3>
+            </article>
           </section>
 
           <section className={styles.contacts}>
             <header>
               <ContactIcon />
-              <h3>Contacts</h3>
+              <h2>Contacts</h2>
             </header>
 
             <ul className={styles.links}>
@@ -114,7 +114,7 @@ const CVPage = () => {
           <section className={styles.socials}>
             <header>
               <ShareIcon />
-              <h3>Socials</h3>
+              <h2>Socials</h2>
             </header>
 
             <ul className={styles.links}>
@@ -144,106 +144,83 @@ const CVPage = () => {
           <section className={styles.work}>
             <header>
               <BriefcaseIcon />
-              <h3>Experience</h3>
+              <h2>Experience</h2>
             </header>
 
-            <div>
-              <span className={styles.date}>2014 - Now</span>
-              <span className={styles.company}>Isfahan, Iran</span>
-              <span className={styles.jobTitle}>Freelance Developer</span>
-              <p className={styles.description}>I've been a freelance developer since 2014. Created an online shopping website
+            <article>
+              <h5>2014 - Now</h5>
+              <h4>Isfahan, Iran</h4>
+              <h3>Freelance Developer</h3>
+              <p>I've been a freelance developer since 2014. Created an online shopping website
                 using pure <b>PHP</b>, <b>HTML</b> and <b>CSS</b> as my first project. Then I registered in a freelancing
                 website and took a couple of iOS mobile applications and created them with <b>Objective-C</b> and <b>Swift</b>.
                 I've built on mobile and desktop games using <b>Unity Engine</b>. I also created full websites.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>Feb 2016 - Mar 2016</span>
-              <span className={styles.company}>Ravis | Isfahan, Iran</span>
-              <span className={styles.jobTitle}>iOS Developer</span>
-              <p className={styles.description}>Ravis is a flight ticket reseller which was providing a platform to sell
+            <article>
+              <h5>Feb 2016 - Mar 2016</h5>
+              <h4>Ravis | Isfahan, Iran</h4>
+              <h3>iOS Developer</h3>
+              <p>Ravis is a flight ticket reseller which was providing a platform to sell
                 charter flight tickets online. They had a fully functional website and I've joined their team to develop
                 an iOS application with <b>Swift</b> to communicate with their APIs and provide the same functionality to
                 their mobile users.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>2016 - 2017</span>
-              <span className={styles.company}>Solar Sky | Iceland</span>
-              <span className={styles.jobTitle}>Front-end Developer</span>
-              <p className={styles.description}>I was working with a team of <b>.NET</b> developers which was working remotely
+            <article>
+              <h5>2016 - 2017</h5>
+              <h4>Solar Sky | Iceland</h4>
+              <h3>Front-end Developer</h3>
+              <p>I was working with a team of <b>.NET</b> developers which was working remotely
                 with Solar Sky which was a cleaning services company. We were working on their internal management panel to
                 make managing their employees and clients easier. It was built on a <b>C#</b> and <b>.NET</b> base and I
                 implemented their UI designs with <b>HTML5</b>, <b>SCSS</b> and <b>jQuery</b>.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>Jul 2017 - Dec 2017</span>
-              <span className={styles.company}>Royal Club | Isfahan, Iran</span>
-              <span className={styles.jobTitle}>Founder & CTO</span>
-              <p className={styles.description}>I've gathered a team and planned to create a mobile cards game with unity.
-                As it was my first experience of starting a team and managing the project, the project failed after 6 months.
-                I've learned a lot about our mistakes that caused the project to fail. I was developing the server side application
-                using <b>Node.js</b> and the rest of the team was designing and implementing the UI and developing the
-                game logic and UI interactions, we used a git server to manage our project code base.</p>
-            </div>
-
-            <div>
-              <span className={styles.date}>Jan 2018 - Aug 2018</span>
-              <span className={styles.company}>Kavoshak | Isfahan, Iran</span>
-              <span className={styles.jobTitle}>Co-Founder & CTO</span>
-              <p className={styles.description}>Just right after failing my last project, I joined another team of my friends
-                to work in their startup project which was a price comparison website that crawls online shops and gathers
-                prices of various products and shows them to the user. It was meant to trace product prices in online shops and
-                make a competition between them and let the user find the lowest price available. I was working on the project CMS
-                using <b>Laravel</b> and also created a website crawler using <b>Python</b>. I quit the team and project when I had
-                to move to WorldSkills preparation camps.</p>
-            </div>
-
-            <div>
-              <span className={styles.date}>Mar 2019 - May 2019</span>
-              <span className={styles.company}>Raadkish | Kish, Iran</span>
-              <span className={styles.jobTitle}>Front-end Developer</span>
-              <p className={styles.description}>During my training for WorldSkills 2019 competition, I've joined <i>Raadkish</i>
+            <article>
+              <h5>Mar 2019 - May 2019</h5>
+              <h4>Raadkish | Kish, Iran</h4>
+              <h3>Front-end Developer</h3>
+              <p>During my training for WorldSkills 2019 competition, I've joined <i>Raadkish</i>
                 as a front-end developer to improve my skills there. They were working on startup fintech and tourist services.
                 I've learned to code in <b>React</b> and <b>React Native</b> to develop the front-end parts of their websites
                 and applications. Working along with <i>Hadi Mostafapour</i> took me to another level of web development. It was a great
                 experience to see how things can be done better and faster, and learn best practices and new technologies
                 in web development.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>Dec 2019 - Jul 2020</span>
-              <span className={styles.company}>Kish Airport | Kish, Iran</span>
-              <span className={styles.jobTitle}>Full-stack Developer</span>
-              <p className={styles.description}>Load and balance has some procedures to find the center of gravity of an
+            <article>
+              <h5>Dec 2019 - Jul 2020</h5>
+              <h4>Kish Airport | Kish, Iran</h4>
+              <h3>Full-stack Developer</h3>
+              <p>Load and balance has some procedures to find the center of gravity of an
                 airplane to use it for taking-off and landing. They were calculating the CG manually on papers, and my
                 job was to created a website to do this automatically. It was created with <b>Laravel</b> and <b>React</b>.
                 A challenging part of this project was its' data entry forms, since there was a ton of nested and related forms
                 which was addressed by creating a form scaffolding structure, to create all those forms based on their database
                 structure. I also had to work with the Airport DCS APIs to get flights and passengers data.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>Jan 2020 - May 2020</span>
-              <span className={styles.company}>Hamresan | Tehran, Iran</span>
-              <span className={styles.jobTitle}>Full-stack Developer</span>
-              <p className={styles.description}>I joined as the first developer to the <i>Hamresan</i> company as a full-stack
+            <article>
+              <h5>Jan 2020 - May 2020</h5>
+              <h4>Hamresan | Tehran, Iran</h4>
+              <h3>Full-stack Developer</h3>
+              <p>I joined as the first developer to the <i>Hamresan</i> company as a full-stack
                 developer. We started a PWA application to provide live flight and cruises schedule, and let users get their
                 flight boarding pass or reserve and buy cruise tickets on their phones. This project was built with <b>Node.js</b>
                 and <b>React</b> and used <b>GraphQL</b> and <b>Socket.io</b> for the communication between the client and the server.</p>
-            </div>
+            </article>
 
-            <div>
-              <span className={styles.date}>May 2020 - Now</span>
-              <span className={styles.company}>Hamresan | Tehran, Iran</span>
-              <span className={styles.jobTitle}>Project Manager & Team Lead</span>
-              <p className={styles.description}>After the company decided to recruit more developers, I've worked as the project
+            <article>
+              <h5>May 2020 - Now</h5>
+              <h4>Hamresan | Tehran, Iran</h4>
+              <h3>Project Manager & Team Lead</h3>
+              <p>After the company decided to recruit more developers, I've worked as the project
                 manager and the team lead, learned to be a <b>Scrum</b> master and manage a project. We have created several
                 web applications since then. A PWA Podcast app, a couple of medical websites and an online learning platform.
                 Meanwhile, I was working as a Back-end developer and implemented a Node.js framework with <b>Typescript</b> which
                 boosts bootstrapping our projects.</p>
-            </div>
+            </article>
           </section>
         </main>
       </div>
