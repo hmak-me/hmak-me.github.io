2d870d4e94f11a6c8dfdbc7bd08aef4f76743977
Hossein Maktoobian <atikalaker@gmail.com>
2020-12-21 11:32:42 +0330

    Make 404 page responsive

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -8,7 +8,8 @@ const NotFoundPage = () => (
   <main className={styles.notFoundPage}>
     <SEO title="Not found" />
 
-    <h2>Are You Lost?</h2>
+    <h2>Uh-Oh!</h2>
+    <p>I'm sorry, but there is no such page in my website, looks like you took the wrong turn!</p>
 
     <Link to='/'>
       <Button>Take Me Home</Button>
